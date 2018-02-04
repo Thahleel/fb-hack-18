02a76a7bd0aca3dc7eeda9d5b810688033c5fa10
simon816 <simonbarnes1@googlemail.com>
2018-02-04 06:32:58 +0000

    Add ics generator

--- server.mjs
+++ server.mjs
@@ -3,6 +3,7 @@ import express from 'express'
 import { setupExpress } from './lib/express'
 import FacebookEvents from './lib/facebook'
 import DescriptionAnalyser from './lib/nlp'
+import { icalGenerator } from './lib/ical'
 
 import logger from './lib/logger'
 
@@ -35,6 +36,15 @@ async function main() {
   })
   server.use('/api', apiRouter)
 
+  server.get('/ics', async (req, res) => {
+    const lat = req.query.lat
+    const lng = req.query.lng
+    const events = await facebook.searchForEvents(lat, lng)
+    const foodEvents = await nlp.filterFoodEvents(events)
+    res.header('Content-Disposition', 'attachment; filename="food_events.ics"')
+    res.header('Content-Type', 'text/calendar')
+    icalGenerator(foodEvents, cal => res.send(cal))
+  })
 
   // Start the server
   const port = process.env.PORT || 8080
